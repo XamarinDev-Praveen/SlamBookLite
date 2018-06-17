f4d849497986e7f4c889da6422b59f5ca2ed80df
XamarinDev-Praveen <37475807+XamarinDev-Praveen@users.noreply.github.com>
2018-06-17 12:06:22 +0530

    testing with double setcontentview code line.

--- app/src/main/java/com/friends/slambooklite/MainActivity.java
+++ app/src/main/java/com/friends/slambooklite/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         //Why its not working
         super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Praveen
     }
